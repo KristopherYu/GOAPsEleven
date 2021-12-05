f05bdca487e6b156c217c4948d47c142bad3e9c2
KristopherYu <74690002+KristopherYu@users.noreply.github.com>
2021-12-04 18:24:02 -0800

    Added constructors for the Dialogue classes

--- Assets/Textures/Dialogue.cs
+++ Assets/Textures/Dialogue.cs
@@ -11,5 +11,18 @@ public class Dialogue
     public bool isButton;
     public int[] score;
 
+    //no values passed in
+    public Dialogue(){
+        name = "";
+        isButton = false;
+    }
+    //Pass in values
+    public Dialogue(string firstName, string[] incomingSentences, bool button, int[] scoreValue){
+        name = firstName;
+        sentences = incomingSentences;
+        isButton = button;
+        score = scoreValue;
+    }
+
 }
 
